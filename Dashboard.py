c9f6c46f4eacb31f01999e3a090a297b0a4eb82c
Jhonatta Silva <jhonatta.ferreira96@outlook.com>
2023-12-28 18:20:22 -0300

    Finished the Dashboard, doing the Raw Data page and adding filters to the dashboard

--- Dashboard.py
+++ Dashboard.py
@@ -16,12 +16,33 @@ st.title('DASHBOARD DE VENDAS :shopping_trolley:')
 
 ## Importar base e formatar campos
 url = 'https://labdados.com/produtos'
-response = requests.get(url)
+regioes = ['Brasil', 'Centro-Oeste', 'Nordeste', 'Norte', 'Sudeste', 'Sul']
+
+st.sidebar.title('Filtros')
+regiao = st.sidebar.selectbox('Região', regioes)
+
+if regiao == 'Brasil':
+    regiao = ''
+    
+todos_anos = st.sidebar.checkbox('Dados de todo o período', value=True)
+if todos_anos:
+    ano = ''
+else:
+    ano = st.sidebar.slider('Ano', 2020, 2023)
+    
+
+query_string = {'regiao': regiao.lower(), 'ano': ano}    
+response = requests.get(url, params=query_string)
 dados = pd.DataFrame.from_dict(response.json())
 dados['Data da Compra'] = pd.to_datetime(dados['Data da Compra'], format='%d/%m/%Y')
 
 
+filtro_vendedores = st.sidebar.multiselect('Vendedores', dados['Vendedor'].unique())
+if filtro_vendedores:
+    dados = dados[dados['Vendedor'].isin(filtro_vendedores)]
+
 ## Tabelas e Medidas
+### Tabelas de receita
 receita_estados = dados.groupby('Local da compra')[['Preço']].sum()
 receita_estados = dados.drop_duplicates(subset='Local da compra')[['Local da compra', 'lat', 'lon']].merge(receita_estados, left_on='Local da compra', right_index=True).sort_values('Preço', ascending=False)
 
@@ -30,6 +51,12 @@ receita_mensal['Ano'] = receita_mensal['Data da Compra'].dt.year
 receita_mensal['Mes'] = receita_mensal['Data da Compra'].dt.month_name()
 
 receita_categorias = dados.groupby('Categoria do Produto')[['Preço']].sum().sort_values('Preço', ascending=False)
+
+
+### Tabelas de quantidade de vendas
+
+### Tabelas vendedores
+vendedores = pd.DataFrame(dados.groupby('Vendedor')['Preço'].agg(['sum', 'count']))
 
 ## Gráficos
 fig_mapa_receita = px.scatter_geo(receita_estados,
@@ -69,15 +96,44 @@ fig_receita_categorias = px.bar(receita_categorias,
 fig_receita_categorias.update_layout(yaxis_title = 'Receita')
 
 ## Dashboard
-col1, col2 = st.columns(2)
+aba1, aba2, aba3 = st.tabs(['Receita', 'Quantidade de vendas', 'Vendedores'])
 
-with col1:
-    st.metric('Receita', formata_numero(dados['Preço'].sum(), 'R$'))
-    st.plotly_chart(fig_mapa_receita, use_container_width = True)
-    st.plotly_chart(fig_receita_estados, use_container_width = True)
-with col2:
-    st.metric('Quantidade de Vendas', formata_numero(dados.shape[0]))
-    st.plotly_chart(fig_receita_mensal, use_container_width = True)
-    st.plotly_chart(fig_receita_categorias, use_container_width = True)
 
-st.dataframe(dados)
+with aba1:
+    col1, col2 = st.columns(2)
+    with col1:
+        st.metric('Receita', formata_numero(dados['Preço'].sum(), 'R$'))
+        st.plotly_chart(fig_mapa_receita, use_container_width = True)
+        st.plotly_chart(fig_receita_estados, use_container_width = True)
+    with col2:
+        st.metric('Quantidade de Vendas', formata_numero(dados.shape[0]))
+        st.plotly_chart(fig_receita_mensal, use_container_width = True)
+        st.plotly_chart(fig_receita_categorias, use_container_width = True)
+
+with aba2:
+    col1, col2 = st.columns(2)
+    with col1:
+        st.metric('Receita', formata_numero(dados['Preço'].sum(), 'R$'))
+    with col2:
+        st.metric('Quantidade de Vendas', formata_numero(dados.shape[0]))
+    
+with aba3:
+    qtd_vendedores = st.number_input('Quantidade de vendedores', 2, 10, 5)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.metric('Receita', formata_numero(dados['Preço'].sum(), 'R$'))
+        fig_receita_vendedores = px.bar(vendedores[['sum']].sort_values('sum', ascending=False).head(qtd_vendedores),
+                                        x='sum',
+                                        y=vendedores[['sum']].sort_values(['sum'], ascending=False).head(qtd_vendedores).index,
+                                        text_auto=True,
+                                        title=f'Top {qtd_vendedores} vendedores (receita)')
+
+        st.plotly_chart(fig_receita_vendedores)
+    with col2:
+        st.metric('Quantidade de Vendas', formata_numero(dados.shape[0]))
+        fig_vendas_vendedores = px.bar(vendedores[['count']].sort_values('count', ascending = False).head(qtd_vendedores),
+                                        x = 'count',
+                                        y = vendedores[['count']].sort_values('count', ascending = False).head(qtd_vendedores).index,
+                                        text_auto = True,
+                                        title = f'Top {qtd_vendedores} vendedores (Quantidade de vendas)')
+        st.plotly_chart(fig_vendas_vendedores, use_container_width = True)
